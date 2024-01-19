20b1ee1f5c9121224fee399d4d4b797666ff54cd
Kylderi Nascimento <kylderi.nascimento@hotmail.com>
2024-01-19 17:32:31 -0300

    feat: Styled Components - config theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+
+import { defaultTheme } from "./themes/default";
+import { GlobalStyle } from "./styles/global.styles";
 
 export function App() {
 
   return (
-    <h1>POMODORO TIMER</h1>
+    <ThemeProvider theme={defaultTheme}>
+
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
