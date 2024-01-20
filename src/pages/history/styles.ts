6e888f4d383fd354a6ce3898b86a843105edfbfd
Kylderi Nascimento <kylderi.nascimento@hotmail.com>
2024-01-20 02:11:03 -0300

    feat: Component Status in page History

--- src/pages/history/styles.ts
+++ src/pages/history/styles.ts
@@ -6,7 +6,7 @@ export const HistoryContainer = styled.main`
   padding: 3.5rem;
   display: flex;
   flex-direction: column;
-  
+
   h1 {
     font-size: 1.5rem;
     color: ${(props) => props.theme['gray-100']};
@@ -58,3 +58,29 @@ export const HistoryList = styled.div`
   }
 
 `;
+
+const STATUS_COLORS = {
+    yellow: 'yellow-500',
+    green: 'green-500',
+    red: 'red-500',
+} as const;
+
+interface StatusProps {
+    statusColor: keyof typeof STATUS_COLORS;
+}
+  
+export const Status = styled.span.withConfig({
+    shouldForwardProp: (prop) => prop === 'statuscolor' || prop === 'children',
+  })<StatusProps>`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  
+    &::before {
+      content: '';
+      width: 0.5rem;
+      height: 0.5rem;
+      border-radius: 50%;
+      background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+    }
+  `;
