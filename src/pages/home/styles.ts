0f135f3c5d781221f0b4218befb6b447519f51b6
Kylderi Nascimento <kylderi.nascimento@hotmail.com>
2024-01-20 01:55:00 -0300

    feat: Page History

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -47,7 +47,7 @@ const BaseInput = styled.input`
 export const TaskInput = styled(BaseInput)`
   flex: 1;
 
-  &::-webkit-calendar-picker-indicator {
+  &::-webkit-calendar-picker-indicator { 
     display: none !important;
   }
 `;
